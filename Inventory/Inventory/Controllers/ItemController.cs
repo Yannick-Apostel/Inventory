bd00e8645e12a59d80dd3e569eee57dddd4eae30
Yannick Apostel <apostelyannick@gmail.com>
2023-06-29 17:07:27 +0200

    prepare Views and Routing

---
+++ Inventory/Inventory/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditItem(int id)
+        {
+            return View();
+        }
+    }
+}
