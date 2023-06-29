3c5d5d1203d205931ee56d7c58eccda9be0d0abd
Yannick Apostel <apostelyannick@gmail.com>
2023-06-29 18:21:33 +0200

    added Items to Db

--- Inventory/Inventory/Controllers/ItemController.cs
+++ Inventory/Inventory/Controllers/ItemController.cs
@@ -1,10 +1,16 @@
-﻿using Inventory.Models;
+﻿using Inventory.Data;
+using Inventory.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Inventory.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public ItemController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,7 +22,26 @@ namespace Inventory.Controllers
         }
         public IActionResult SaveItem(Item item)
         {
-            //ToDo write Item to DB
+            
+            if(item.Id == 0) //add new Item if not editing
+            {
+            _dbContext.Components.Add(item);
+            _dbContext.SaveChanges();
+            }
+            else
+            {
+                var itemFromDb = _dbContext.Components.SingleOrDefault(x => x.Id == item.Id);
+                
+                if(itemFromDb == null)
+                {
+                    return NotFound();
+                }
+
+                itemFromDb.Name = item.Name;
+                itemFromDb.count = item.count;
+                itemFromDb.Description = item.Description;
+            }
+            
 
             return RedirectToAction("Index");
         }
