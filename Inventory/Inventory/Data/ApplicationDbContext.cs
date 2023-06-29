823c6d0fac05546bd58853f256b13e3c215c4764
Yannick Apostel <apostelyannick@gmail.com>
2023-06-29 16:51:04 +0200

    create Itemmodel

--- Inventory/Inventory/Data/ApplicationDbContext.cs
+++ Inventory/Inventory/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Inventory.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 
 namespace Inventory.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Item> Components { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ Inventory/Inventory/Data/Migrations/20230629145005_ItemToTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemToTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Components",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    count = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Components", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Components");
+        }
+    }
+}
