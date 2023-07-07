bce0c472b29bd0013746acdf9dafe2ebe0ee8f16
Yannick Apostel <apostelyannick@gmail.com>
2023-07-08 00:28:27 +0200

    add AccountManager
    in the future there should be several subaccounts for one main account

---
+++ Inventory/Inventory/Controllers/AccountManagerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Controllers
+{
+	public class AccountManagerController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult CreateUser()
+		{
+			return View();
+		}
+
+		public IActionResult ShowUser()
+		{
+			return View();
+		}
+
+		public IActionResult Settings()
+		{
+			return View();
+		}
+	}
+}
---
+++ Inventory/Inventory/Models/AccountManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory.Models
+{
+	public class AccountManager
+	{
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public string Sirname { get; set; }
+		public string Name { get; set; }
+		public string CreatingUser { get; set; }
+	}
+}
